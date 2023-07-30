ea223cef99c75de36135e89370a1bedb2e023dc5
bgpooja56 <poojabg@Poojas-MacBook-Pro.local>
2023-07-30 20:33:51 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {
+    id: string;
+    title: string;
+    location: string;
+    image: any;
+    details: string;
+    interiors: any[];
+};
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Trending Products',
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            title: 'Trending Details',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
